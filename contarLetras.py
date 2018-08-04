6ad92187175b30c29a44c0f6a38e2adc0275a2bd
Responses <sam.98Panton@hotmail.com>
2018-08-04 01:30:04 -0600

    Add files via upload

---
+++ contarLetras.py
@@ -0,0 +1,7 @@
+#Contador de palabras
+def contarLetras(palabra):
+	contador=0
+	for i in palabra:
+	    contador+=1
+	print("La palabra "+palabra +" tiene "+str(contador) +" letras en total")
+    
